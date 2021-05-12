eeb0eb3e8f0ad23ac1fd2cf178fa8394095d52ce
Văn Khánh Vương <vankhanhvuong98@gmail.com>
2021-05-12 11:35:36 +0700

    feat: commit code UI project

--- W9-Libary-Alamofire/Screen/DetailView/DetailViewController.swift
+++ W9-Libary-Alamofire/Screen/DetailView/DetailViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  DetailViewController.swift
 //  W9-Libary-Alamofire
 //
 //  Created by Văn Khánh Vương on 12/05/2021.
@@ -7,13 +7,9 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class DetailViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
 }
-
---
+++ W9-Libary-Alamofire/Screen/PhotoView/Parts/Cell/PhotoTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoTableViewCell.swift
+//  W9-Libary-Alamofire
+//
+//  Created by Văn Khánh Vương on 12/05/2021.
+//
+
+import UIKit
+
+class PhotoTableViewCell: UITableViewCell {
+    @IBOutlet weak var namePhotoTopicLabel: UILabel!
+    @IBOutlet weak var photoTopicImageView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+}
